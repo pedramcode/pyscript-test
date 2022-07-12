cd59f1a0faca77233f78a1f11d85d7257d18eb10
pedramcode <dev.dehghanpour@gmail.com>
2022-07-12 23:36:24 +0430

    server - pyscript

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
-def helloWorld():
-  return "Hello, cross-origin-world!"
+def home():
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
